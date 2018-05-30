54db1acd04c87e201bda0e476108ed8f848a95c7
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-05-30 11:55:24 +0300

    added log before lunch

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello New World!" );
+        System.out.println( "Hello Newest World!" );
     }
 }
